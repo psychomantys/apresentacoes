b999ed1fb5aae428fde73af2a6237403820d95d0
psychomantys <psycho.mantys@gmail.com>
2012-08-28 15:51:38 -0300

    Initial commit

---
+++ 2011/mini_curso-ic_openmp_mpi/codigo/torque-submit-openmp.sh
@@ -0,0 +1,11 @@
+# Metodo normal
+ssh s10n01 ./executavel arg1 arg2 ... argN
+
+# Novo metodo
+torque-openmp 4 ./executavel arg1 arg2 ... argN
+
+# Saidas do programa
+ls STDIN.*
+STDIN.o20       STDIN.e20
+
+
