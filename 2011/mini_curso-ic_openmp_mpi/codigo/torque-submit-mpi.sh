78a088fb4afb6f683f59dcc9ab391d391a8eb0c1
psychomantys <psycho.mantys@gmail.com>
2012-08-29 18:34:38 -0300

    reorganizando.

--- 2011/mini_curso-ic_openmp_mpi/codigo/torque-submit-mpi.sh
+++
@@ -1,9 +0,0 @@
-# Metodo antigo
-mpirun -np 4 ./executavel arg1 arg2 ... argN
-
-# Novo metodo
-qsub -l nodes=4:ppn=8 <<<"mpirun ./executavel arg1 arg2 ... argN"
-
-# Saidas do programa
-ls STDIN.*
-STDIN.o19	STDIN.e19
