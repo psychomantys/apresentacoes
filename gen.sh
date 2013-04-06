e357f65fd5edf9a59c4af5db18ba8da67646fbcf
psychomantys <psycho.mantys@gmail.com>
2013-04-06 01:48:38 -0300

    Pequeno bug na parte de gerar as paginas, agora esta certo.

--- gen.sh
+++ gen.sh
@@ -6,10 +6,21 @@ fi
 
 mkindex(){
 # usage: auto-index [dir]
+
+	local directory=
+
+	if [ ! "$1" ] ; then
+		directory=.
+	else
+		directory="$1"
+	fi
+
+	(
+	cd "${directory}"
 	echo "<html>
-<head><title>Index of $1</title></head>
+<head><title>Index of ${directory}</title></head>
 <body>
-<h2>Index of $1</h2>
+<h2>Index of ${directory}</h2>
 <a href=../ls_index.html>Parent Dir.</a>
 <hr>
 <ui>
@@ -17,10 +28,10 @@ mkindex(){
 	for x in * ; do
 		#INDEX=`ls -1 $1 | sed "s/^.*/      <li\>\<a\ href=\"&\"\>&\<\\/a\>\<\\/li\>/"`
 		if [ -d "${x}" ] ; then
-			echo "<li><a href=\"${x}/ls_index.html\">[DIR]      ${x}/</a></li>"
+			echo "<li><a href=\"${x}/ls_index.html\">[DIR] &nbsp;&nbsp;&nbsp;&nbsp; ${x}/</a></li>"
 		else
 			if [ "$x" != "ls_index.html" ] ; then
-				echo "<li><a href=\"${x}\">[FILE]     ${x}</a></li>"
+				echo "<li><a href=\"${x}\">[FILE] &nbsp;&nbsp;&nbsp; ${x}</a></li>"
 			fi
 		fi
 	done
@@ -28,13 +39,13 @@ mkindex(){
 <ui>
 </body>
 </html>"
+	)
 }
 
 ndx=ls_index.html
 find "$1" -type d | while read dir ; do
 (
-	cd "$dir" &&
-	mkindex > "$ndx"
+	mkindex "${dir}" > "${dir}/${ndx}"
 	## where mkindex is a script to create index file
 )
 done
