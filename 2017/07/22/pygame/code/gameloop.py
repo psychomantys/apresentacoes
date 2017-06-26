d8cc29ed83858fcd21197a511a934a78e56624b2
psychomantys <psycho.mantys@gmail.com>
2017-06-26 01:15:55 -0300

    Slides sobre display e mudancas no codigo de teste

--- 2017/07/22/pygame/code/gameloop.py
+++ 2017/07/22/pygame/code/gameloop.py
@@ -7,19 +7,33 @@ O codigo apenas abre uma tela e trata o evento de QUIT
 """
 
 import pygame
-import os 
+import sys
+import os
 
 
 # Variaveis usadas ao longo do programa
 done=False
-clock=pygame.time.Clock()
+clock=None
 screen=None
 fps=30
 screen_mode=(800,600)
 
+def main(argv):
+	try:
+		game_init()
+		while not done:
+			event_handler()
+			game_logic()
+			step_physics()
+			render()
+	finally:
+		game_end()
+
+
 
 def event_handler():
 	global done
+
 	for event in pygame.event.get():
 		# Trata evento QUIT
 		if event.type == pygame.QUIT:
@@ -28,8 +42,11 @@ def event_handler():
 
 def game_init():
 	pygame.init()
-	screen=pygame.display.set_mode(screen_mode)
-	done=True
+	pygame.display.set_caption("Python Day")
+	pygame.display.set_mode(screen_mode)
+
+	global clock
+	clock=pygame.time.Clock()
 
 
 def game_end():
@@ -37,26 +54,20 @@ def game_end():
 
 
 def render():
+	pygame.display.flip()
+
 	# Limita as iterações pelo numero de FPS
 	clock.tick(fps)
-	pygame.display.flip()
 
 
 def game_logic():
-	done
+	pass
 
 
 def step_physics():
-	done
+	pass
 
 
-try:
-	game_init()
-	while not done:
-		event_handler()
-		game_logic()
-		step_physics()
-		render()
-finally:
-	game_end()
+if __name__ == "__main__":
+	main(sys.argv)
 
