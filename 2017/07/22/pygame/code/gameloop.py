b452a4b2c063da65c6638d4fde07c4803d9b4793
psychomantys <psycho.mantys@gmail.com>
2017-07-06 02:59:19 -0300

    quase terminando apresentação, so falta a parte de group e revisar

--- 2017/07/22/pygame/code/gameloop.py
+++ 2017/07/22/pygame/code/gameloop.py
@@ -3,7 +3,6 @@ Codigo demonstrativo para a apresentação. Algumas coisas podem
 ser melhores, mas para a apresentação acho que vai ser mais didatico
 assim
 
-O codigo apenas abre uma tela e trata o evento de QUIT
 """
 
 import pygame
@@ -15,8 +14,54 @@ import os
 done=False
 clock=None
 screen=None
+layer_front=None
+
+speed_x=5
+speed_y=5
+color_transparent=(44,3,195)
+color_background=(0,0,0)
 fps=30
 screen_mode=(800,600)
+
+class Ball(pygame.sprite.Sprite):
+	image=None
+	rect=None
+	speed_x=None
+	speed_y=None
+
+	def __init__(self, image, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image=pygame.image.load(image).convert()
+		self.image.set_colorkey(color_transparent)
+		self.rect=self.image.get_rect()
+
+		self.rect.x=x
+		self.rect.y=y
+
+		self.speed_x=speed_x
+		self.speed_y=speed_y
+
+	def draw(self, screen):
+		screen.blit(self.image, self.rect)
+
+	def move(self, x, y):
+		self.rect.move_ip(x,y)
+
+	def turn(self):
+		self.image=pygame.transform.flip(self.image, True, False)
+
+	def update(self):
+		if self.rect.left<0:
+			self.speed_x=-self.speed_x
+			self.turn()
+		if self.rect.right>800:
+			self.speed_x=-self.speed_x
+			self.turn()
+		if self.rect.top<0:
+			self.speed_y=-self.speed_y
+		if self.rect.bottom>600:
+			self.speed_y=-self.speed_y
+
 
 def main(argv):
 	try:
@@ -44,20 +89,32 @@ def event_handler():
 
 
 def game_init():
+	global screen
+	global clock
+	global layer_front
+
 	pygame.init()
 	pygame.display.set_caption("Python Day")
 	screen=pygame.display.set_mode(screen_mode)
 
-	global clock
 	clock=pygame.time.Clock()
-	screen.fill([255, 255, 255])
+	screen.fill(color_background)
+
+	layer_front=pygame.sprite.RenderUpdates()
+
+	ball=Ball("figuras/ball.png", 100, 100)
+	layer_front.add(ball)
+	ball=Ball("figuras/ball.png", 200, 450)
+	layer_front.add(ball)
 
 
 def game_end():
 	pygame.quit()
 
-
 def render():
+	screen.fill(color_background)
+	#ball.draw(screen)
+	layer_front.draw(screen)
 	pygame.display.flip()
 
 	# Limita as iterações pelo numero de FPS
@@ -65,12 +122,14 @@ def render():
 
 
 def game_logic():
-	pass
+	# ball.move(speed_x, speed_y)
+	for item in layer_front:
+		item.move(item.speed_x, item.speed_y)
 
 
 def step_physics():
-	pass
-
+	# phys.step()
+	layer_front.update()
 
 if __name__ == "__main__":
 	main(sys.argv)
